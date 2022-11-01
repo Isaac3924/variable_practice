ef3d8f047ebaedf4aff5c1336e1376ea8b0caa72
Isaac Alter <i_alter@yahoo.com>
2022-11-01 15:10:57 -0600

    Reassign string variable values and add  7 puts.

--- variables.rb
+++ variables.rb
@@ -13,3 +13,15 @@ float_3 = 72.03
 bool_1 = true
 bool_2 = true
 bool_3 = false
+
+string_var_1 = "Hello to you too!"
+string_var_2 = "Make me"
+string_var_3 = "no u"
+
+puts "How y'all doing tonight?"
+puts "'Cause I'm feeling nothing."
+puts "And it's been that way since middle school!"
+puts string_var_3
+puts float_2
+puts bool_3
+puts int_1
